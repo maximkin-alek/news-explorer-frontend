e6c575580bc2f22bc62eb3674223d7ad7e0556a3
Alexander Maximkin <maximkin.alek@yandex.ru>
2020-10-15 22:55:42 +0300

    fix bug

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              publicPath: '../',
+
               name: './images/[name].[ext]',
               esModule: false
             }
